6da30fa3be0717f445d8889021cb61807b011bfc
Mczepcz <michal.czepczynski@gmail.com>
2016-06-23 18:53:52 +0200

    Main controller was made, Bootstrap CSS was added, some twigs was made

--- deutsche_lehrer/src/AppBundle/Controller/MainController.php
+++ deutsche_lehrer/src/AppBundle/Controller/MainController.php
@@ -5,14 +5,25 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class MainController extends Controller
 {
     /**
-     * @Route("/", name="mainPage")
-     * @Template("name")
+     * @Route("/", name="landingPage")
+     * @Template("AppBundle:Main:landingPage.html.twig")
+     */
+    public function showLandPageAction(){
+        return array();
+    }
+    /**
+     * @Route("/main", name="mainPage")
+     * @Template("AppBundle:Main:mainPage.html.twig")
+     * @Security("has_role('ROLE_USER')")
      */
     public function showMainPageAction(){
+        $loggedUser = $this->getUser();
+        return array("name"=>$loggedUser->getUsername());
         
     }
 }
